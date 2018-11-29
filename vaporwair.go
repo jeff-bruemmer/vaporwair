101a6ba1a8a2c9c3871822d5031770f1bb9416ad
Jeff Bruemmer <jeff.bruemmer@gmail.com>
2018-11-29 08:53:11 -0800

    GetWeatherForecast.

--- vaporwair.go
+++ vaporwair.go
@@ -1,44 +1,35 @@
 package main
 
 import (
+	"compress/gzip"
 	"encoding/json"
 	"fmt"
 	"github.com/jeff-bruemmer/vaporwair/dialer"
 	"github.com/jeff-bruemmer/vaporwair/geolocation"
 	"github.com/jeff-bruemmer/vaporwair/storage"
+	"github.com/jeff-bruemmer/vaporwair/weather"
 	"log"
 	"os"
 	"time"
 )
 
-// // Dials IP-API to obtain geolocation data
-// func GetGeoData(address string) {
-// 	resp, err := dialer.NetReq(dialer.IPAPIAddress, 5, false)
-// 	if err != nil {
-// 		fmt.Println("There was a problem obtaining your coordinates.")
-// 	}
-// 	var geoData geolocation.GeoData
-// 	defer resp.Body.Close()
-// 	json.NewDecoder(resp.Body).Decode(&geoData)
-// }
-
 func buildDarkSkyURL(addr string, apikey string, c geolocation.Coordinates, units string) string {
 	return addr +
 		apikey +
 		"/" +
-		fmt.Sprintf("%f", c.Latitude) +
+		c.Latitude +
 		"," +
-		fmt.Sprintf("%f", c.Longitude) +
+		c.Longitude +
 		"?units=" +
 		units
 }
 
 // GetGeoData dials the IP-API server to obtain geolocation data
 // based on user's IP address.
-func GetGeoData() (geolocation.GeoData, error) {
+func GetGeoData(addr string) (geolocation.GeoData, error) {
 	var gd geolocation.GeoData
 	// Request coordinates from ip-api and specify timeout in seconds
-	resp, err := dialer.NetReq("http://ip-api.com/json", 2, false)
+	resp, err := dialer.NetReq(addr, 5, false)
 	if err != nil {
 		return gd, err
 	}
@@ -53,11 +44,36 @@ func GetGeoData() (geolocation.GeoData, error) {
 
 }
 
-func main() {
-	// Print time (also signals to user that program is running).
+// GetWeatherForecast dials the Dark Sky API and returns a weather forecast.
+func GetWeatherForecast(addr string) (weather.Forecast, error) {
+	var wf weather.Forecast
+	// Request coordinates from ip-api and specify timeout in seconds
+	// Set gzip bool to true.
+	resp, err := dialer.NetReq(addr, 5, true)
+	if err != nil {
+		return wf, err
+	}
+	// Unzip response
+	defer resp.Body.Close()
+	gz, err := gzip.NewReader(resp.Body)
+	if err != nil {
+		fmt.Println("Error decoding gzip response from Dark Sky API.")
+		log.Fatal(err)
+	}
+	// Decode unzipped response into weather forecast.
+	defer gz.Close()
+	json.NewDecoder(gz).Decode(&wf)
+	return wf, err
+}
+
+// printTime prints time and signals to user that program has started.
+func printTime() {
 	t := time.Now()
 	fmt.Println(t.Format("Mon Jan 2 15:04:05 MST 2006"))
+}
 
+func main() {
+	printTime()
 	// First get home directory for user.
 	homeDir, err := storage.GetHomeDir()
 	if err != nil {
@@ -75,15 +91,16 @@ func main() {
 
 	// Get Coordinates from IP-API
 	var geoData geolocation.GeoData
-	geoData, err = GetGeoData()
+	geoData, err = GetGeoData(dialer.IPAPIAddress)
 	if err != nil {
-		fmt.Println("There was a problem obtaining your coordinates")
+		fmt.Println("There was a problem obtaining your coordinates.")
 		log.Fatal(err)
 	}
-	fmt.Println(geoData)
-
+	coordinates := geolocation.FormatCoordinates(geoData)
+	fmt.Println(coordinates)
 	// build DarkSkyURL
-
+	dsURL := buildDarkSkyURL(dialer.DarkSkyAddress, config.DarkSkyAPIKey, coordinates, dialer.DarkSkyUnits)
+	fmt.Println(dsURL)
 	// build AirNowURL
 
 	// If cached forecast has expired, dial IP-API call
@@ -95,6 +112,11 @@ func main() {
 	// If coordinates returned by IP-API call differ from coordinates in saved forecast,
 	// user is in a new location, and calls with the updated coordinates need to be made.
 	// 1. Dial Dark Sky API
+	wf, err := GetWeatherForecast(dsURL)
+	if err != nil {
+		log.Fatal("There was a problem obtaining the weather forecast.")
+	}
+	fmt.Println(wf)
 	// 2. Dial AirNow API
 
 	// Select fastest valid forecast that returns
